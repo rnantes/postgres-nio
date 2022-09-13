3e41b82d06fababae0bdfa2dbc0a8fd5f22e5fea
rnantes <reid.nantes@gmail.com>
2022-09-13 17:08:53 -0400

    support v10_15

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "postgres-nio",
     platforms: [
-        .macOS(.v13),
+        .macOS(.v10_15),
         .iOS(.v13),
         .watchOS(.v6),
         .tvOS(.v13),
